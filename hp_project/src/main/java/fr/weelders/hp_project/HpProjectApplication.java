805925230ac992cf1f68ef1d021f96f558ad5954
Rodolphe <weelders@hotmail.fr>
2022-07-06 19:21:07 +0200

    Add lib String + add thymleaf template for char editing & dislaying + add console logs

--- hp_project/src/main/java/fr/weelders/hp_project/HpProjectApplication.java
+++ hp_project/src/main/java/fr/weelders/hp_project/HpProjectApplication.java
@@ -7,20 +7,23 @@ import fr.weelders.hp_project.Bean.Taille;
 import fr.weelders.hp_project.DAO.GetAllDAO;
 import fr.weelders.hp_project.DAO.GenerateDAO;
 import fr.weelders.hp_project.DAO.GetIdDAO;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import static fr.weelders.hp_project.DAO.GetAllDAO.getPersonnageList;
 import static fr.weelders.hp_project.DAO.GetAllDAO.getTailleList;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class HpProjectApplication
 {
 
@@ -37,30 +40,37 @@ public class HpProjectApplication
     @GetMapping("/getAllMaison")
     public ArrayList<Maison> getAllMaison() throws SQLException
     {
+        Utils.consoleLog("/getAllMaison");
         return GetAllDAO.getMaisonList();
     }
 
     @GetMapping("/getAllBaguette")
     public ArrayList<Baguette> getAllBaguette() throws SQLException
     {
+        Utils.consoleLog("/getAllBaguette");
         return GetAllDAO.getBaguetteList();
     }
 
     @GetMapping("/getPersonnageById")
-    public Personnage getPersonnageById(@RequestParam(required = true) int idPerso) throws SQLException
+    public String getPersonnageById(Model model, @RequestParam(required = true) int idPerso) throws SQLException
     {
-        return GetIdDAO.getPersonnageById(idPerso);
+        Utils.consoleLog("/getPersonnageById","idPerso= "+idPerso);
+        Personnage perso = Objects.requireNonNull(GetIdDAO.getPersonnageById(idPerso));
+        model.addAttribute(perso);
+        return "personnage";
     }
 
     @GetMapping("/getAllPersonnage")
     public ArrayList<Personnage> getAllPersonnage() throws SQLException
     {
+        Utils.consoleLog("/getAllPersonnage");
         return GetAllDAO.getPersonnageList();
     }
 
     @GetMapping("/generateBaguette")
     public String generateAllBaguette() throws SQLException
     {
+        Utils.consoleLog("/generateBaguette");
         GenerateDAO.generateBaguette();
         return "ENDS";
     }
@@ -68,14 +78,17 @@ public class HpProjectApplication
     @GetMapping("/generateTaille")
     public ArrayList<Taille> generateAllTaille(@RequestParam(required = true) int min, int max) throws SQLException
     {
+        Utils.consoleLog("/generateTaille","min= "+min+" max= "+max);
         GenerateDAO.generateTaille(min,max);
         return getTailleList();
     }
 
     @GetMapping("/addRandomPerso")
-    public Personnage addRandomPerso(@RequestParam(required = true) Boolean is_pj, String nom_perso, String prenom_perso, String img_perso, byte age_perso, Boolean etat_perso, String desc_perso) throws SQLException
+    public String addRandomPerso(Model model, @RequestParam(required = true) Boolean is_pj, String nom_perso, String prenom_perso, String img_perso, byte age_perso, Boolean etat_perso, String desc_perso) throws SQLException
     {
+        Utils.consoleLog("/addRandomPerso","is_pj= "+is_pj+" nom_perso= "+nom_perso+" prenom_perso= "+prenom_perso+" img_perso= "+img_perso+" age_perso= "+age_perso+" etat_perso= "+etat_perso+" desc_perso= "+desc_perso);
         GenerateDAO.generatePerso(is_pj,nom_perso,prenom_perso,img_perso,age_perso,etat_perso,desc_perso);
-        return getPersonnageById(getPersonnageList().size());
+        model.addAttribute(Objects.requireNonNull(GetIdDAO.getPersonnageById(getPersonnageList().size())));
+        return "personnage";
     }
 }
--- hp_project/src/main/java/fr/weelders/hp_project/Utils.java
+++ hp_project/src/main/java/fr/weelders/hp_project/Utils.java
@@ -1,6 +1,34 @@
 package fr.weelders.hp_project;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class Utils
 {
+    /*--------------------------------------------*/
+    /*--COLORS--*/
+    /*--------------------------------------------*/
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RED = "\u001B[31m";
 
+    /*--------------------------------------------*/
+    /*--FUNCTIONS--*/
+    /*--------------------------------------------*/
+    public static String convertLongToTimeString(long time)
+    {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(time));
+    }
+    public static void consoleLog(String tag, String msg)
+    {
+        long now = Instant.now().toEpochMilli();
+        System.out.println(convertLongToTimeString(now)+ANSI_YELLOW+" --- "+tag+" --- "+ANSI_GREEN+msg+ANSI_RESET);
+    }
+    public static void consoleLog(String tag)
+    {
+        long now = Instant.now().toEpochMilli();
+        System.out.println(convertLongToTimeString(now)+ANSI_YELLOW+" --- "+tag+" --- "+ANSI_RESET);
+    }
 }
